af3dbdcde7744f90d062a04784a54dcf36fdceae
flohmann <franziska.lohmann@gmx.net>
2014-10-21 22:08:23 +0200

    Contract Projekt angelegt und die ersten Spezifikationen gemacht

---
+++ ContractTest/ContractTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using System.Collections;
+using ContractTest;
+
+namespace ContractTest
+{
+
+
+}
+
+
+    class Program
+    {
+        private String ip;
+        private String port;
+        private Boolean connect = false;
+        private ArrayList buffer = new ArrayList();
+
+
+        // CONNECTOR 
+
+        public Boolean isConnect(String ip, String port){
+            Contract.Requires(port != null || port == "666");
+            Contract.Requires(ip != null);    
+
+            Contract.Ensures(connect = true);
+            return connect;
+        }
+
+
+
+        public void pushMessageIntoBuffer(ArrayList buffer, String message){
+            Contract.Requires(buffer.Count >= 0);
+            Contract.Requires(message != null);
+            
+
+            Contract.Ensures(buffer.Contains(message));
+            Contract.Ensures(buffer.Count > 0);
+
+        }
+
+       public void sendMessageToServer(String message){
+           Contract.Requires(message != null);
+
+           Contract.Ensures(message=="INVALID");   // noch nicht fertig!!
+       }     
+
+
+        // PARSER
+
+        public void readBuffer(ArrayList buffer){
+            Contract.Requires(buffer.Count>0);
+
+            Contract.Ensures(buffer.Count==0);
+        }
+
+
+
+
+        //BUFFER
+
+        //BACKEND
+
+        //FRONTEND
+    
+       public static void Main(string[] args)
+        {
+        }
+    }
+}
