e55c5469aa8d2b11480eaebd17c5026602252974
OhNoMelon <nastasja.sekulic@student.reutlingen-university.de>
2014-10-29 18:05:54 +0100

    AufgabErgenzung der Eigenschaften

--- ContractTest/ContractTest/Buffer.cs
+++ ContractTest/ContractTest/Buffer.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using System.Collections;
+using ContractTest;
+
 
 namespace ContractTest
 {
     class Buffer
     {
+
+        private List<String> buffer = new List<String>;
+       
+
+        public Buffer(){
+
+        }
+
+        public void addLineToBuffer(String message)
+        {
+            Contract.Requires(buffer.Count >= 0);
+            Contract.Requires(message != null);
+
+            Contract.Ensures(buffer.Contains(message));
+            Contract.Ensures(buffer.Count == Contract.OldValue((buffer.Count) + 1));
+        }
+
+        public String getLineFromBuffer()
+        {
+            Contract.Requires(buffer.Count > 0);
+
+            Contract.Ensures(buffer.Count == Contract.OldValue((buffer.Count) - 1));
+            return buffer[0];
+        }
+
+        public void bufferContent()
+        {
+            Contract.Requires(buffer.Count >= 0);
+
+            Contract.Ensures(buffer.Count==0);
+        }
+
     }
 }
