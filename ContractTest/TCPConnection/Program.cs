e10468c3ac5363e7c63af70a3c849892ce44d40c
JoTiZ <spam.bunker@ymail.com>
2014-11-03 20:22:44 +0100

    TCPConnetion prograe (ex. no.1 fix)

---
+++ ContractTest/TCPConnection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPConnection
+{
+    class Program
+    {
+        private static TcpClient tcpClient;
+        private static StreamWriter sw;
+        private static StreamReader sr;
+        
+
+        static void Main(string[] args)
+        {
+            tcpClient = new TcpClient();
+            tcpClient.Connect("127.0.0.1", 666);
+
+            //connect the streams (write and read)
+            sw = new StreamWriter(tcpClient.GetStream());
+            sr = new StreamReader(tcpClient.GetStream());
+
+
+            Receiver rec = new Receiver(tcpClient, sr);
+            rec.Receive();
+            Sender sender = new Sender(tcpClient, sw);
+
+            while (tcpClient.Connected)
+            {
+                sender.SendMsg(Console.ReadLine());
+            }
+        }    
+    }
+}
