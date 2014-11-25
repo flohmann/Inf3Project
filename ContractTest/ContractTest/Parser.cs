26ee5a36d355a5145c12bbe818d3b73611ff9708
OhNoMelon <nastasja.sekulic@student.reutlingen-university.de>
2014-11-25 21:24:07 +0100

    startet toconvert the player

--- ContractTest/ContractTest/Parser.cs
+++ ContractTest/ContractTest/Parser.cs
@@ -15,7 +15,11 @@ namespace Inf3Project
     {
         private Backend backend;
         private Buffer buffer;
+        private List<String> msg;
+        private int id;
+        private String type;
 
+        List<String> message;
         public Parser(Buffer buffer){
             backend = new Backend();
             this.buffer = buffer;
@@ -24,14 +28,46 @@ namespace Inf3Project
             Thread readBufferThread = new Thread(new ThreadStart(readBuffer));
             readBufferThread.Start();
         }
-
+        
         public void readBuffer()
         {
-            buffer.getMessageFromBuffer();
-            
+            msg = buffer.getMessageFromBuffer();
+            String[] tmp = msg[0].Split(':');
+            int value ;
+            if ((tmp[0].Equals("begin")) && (Int32.TryParse(tmp[1], out value)))
+            {
+                value=Int32.Parse(tmp[1]);
+                tmp = msg[msg.Count()-1].Split(':');
+                if((tmp[0].Equals("end")) && (Int32.TryParse(tmp[1], out value)))
+                {
+                    msg.RemoveAt(0);
+                    msg.RemoveAt(msg.Count-1);
+                }
+                //delete upd 
+            }
+            tmp = msg[1].Split(':');
+            if((tmp[0].Equals("begin")) && (tmp[1].Equals("player"))){
+                playerConvert(msg);
+
+            }
+    }
+
+        private void playerConvert(List<String> msg)
+        {
+            String [] tmp = msg[0].Split(':');
+            //beginn:player delete and  adapt msg.Count  
+            if (msg.Count == 9)
+            {
+                if(tmp[0].Equals("id")){
+                   id = Int32.Parse(tmp[1]);
+                }
+                tmp = msg[1].Split(':');
+                    if(tmp[0].Equals("type")){
+                        type = tmp[1];
+                    }
+            }
         }
 
-
-        
-    }
+       
+}
 }
