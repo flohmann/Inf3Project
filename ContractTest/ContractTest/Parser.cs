383f3c4de0b52db965a432040b9e6330c025498d
OhNoMelon <nastasja.sekulic@student.reutlingen-university.de>
2014-11-28 10:45:10 +0100

    some EBNF added

--- ContractTest/ContractTest/Parser.cs
+++ ContractTest/ContractTest/Parser.cs
@@ -41,7 +41,7 @@ namespace Inf3Project
         private int ver = -1;
         private DateTime time;
         private int yourId;
-
+        private int online;
         public Parser(Buffer buffer)
         {
             backend = new Backend();
@@ -145,6 +145,13 @@ namespace Inf3Project
                 msg.RemoveAt(msg.Count - 1);
                 parseEntity();
             }
+
+            else if ((tmp[0].Equals("begin")) && ((tmp[1].Equals("ents"))))
+            {
+                msg.RemoveAt(0);
+                msg.RemoveAt(msg.Count - 1);
+                getEBNF();
+            }
             else if ((tmp[0].Equals("begin")) && ((tmp[1].Equals("map"))))
             {
                 msg.RemoveAt(0);
@@ -162,7 +169,7 @@ namespace Inf3Project
                 msg.RemoveAt(0);
                 msg.RemoveAt(msg.Count - 1);
                 parseChallenge();
-                
+
             }
             else if ((tmp[0].Equals("begin")) && ((tmp[1].Equals("time"))))
             {
@@ -183,7 +190,15 @@ namespace Inf3Project
             {
                 msg.RemoveAt(0);
                 msg.RemoveAt(msg.Count - 1);
-                parserYourId();
+                parseYourId();
+
+            }
+
+            else if ((tmp[0].Equals("begin")) && ((tmp[1].Equals("online"))))
+            {
+                msg.RemoveAt(0);
+                msg.RemoveAt(msg.Count - 1);
+                parseOnline();
 
             }
             //every possible ENBF command needs its own if
@@ -476,7 +491,28 @@ namespace Inf3Project
                 throw new Exception("There is no id");
             }
         }
-       
+        private void parseOnline()
+        {
+            String[] tmp = msg[0].Split(':');
+
+            if (tmp[0].Equals("online"))
+            {
+                this.online = Int32.Parse(tmp[1]);
+                msg.RemoveAt(0);
+                tmp = msg[0].Split(':');
+                if ((tmp[0].Equals("end")) && ((tmp[1].Equals("online"))))
+                {
+                    backend.setOnline(online);
+
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+        
+                
         private void createPlayer()
         {
             //used variables - int id, String type, bool busy, String desc, int x, int y, int points
@@ -555,6 +591,7 @@ namespace Inf3Project
             this.accepted = false;
             this.delete = false;
             this.ver = -1;
+            this.online = 0;
         }
 
     }
