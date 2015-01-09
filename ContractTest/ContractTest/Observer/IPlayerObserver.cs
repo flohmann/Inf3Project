3a9dc7924e33a51468647c94e098385f407b10c6
sulo32 <s-coskun32@hotmail.de>
2015-01-09 15:37:19 +0100

    commit for other

---
+++ ContractTest/ContractTest/Observer/IPlayerObserver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf3Project.Observer
+{
+    interface IPlayerObserver: IObserver
+    {
+        public void OnChangePoints(Player p, int point);
+        public void OnChangePosition(Player p, int x, int y);
+        public void OnBusy(Player p, bool busy);
+    }
+}
