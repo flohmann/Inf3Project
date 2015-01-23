3d9b9c3122f48755bcce544ea34fd60a27818bdc
JoTiZ <spam.bunker@ymail.com>
2015-01-23 09:45:11 +0100

    Button desc changed

--- ContractTest/ContractTest/Game/DefaultGui.Designer.cs
+++ ContractTest/ContractTest/Game/DefaultGui.Designer.cs
@@ -362,7 +362,7 @@ namespace Inf3Project
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 23);
             this.button1.TabIndex = 11;
-            this.button1.Text = "Quit Game";
+            this.button1.Text = "Disconnect";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
